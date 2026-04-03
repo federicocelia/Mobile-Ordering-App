09598212ea3aac2c987976e9ca0be3b32f4af388
Federico Celia <73991066+federicocelia@users.noreply.github.com>
2026-04-03 11:27:11 +0100

    create single item container, basic style for menu section and item container

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 import { menuArray } from "./data.js";
 
-console.log(menuArray);
+//console.log(menuArray);
+
+const menuContainer = document.querySelector(".menu-selection");
+const orderContainer = document.querySelector(".order-summary");
+const paymentContainer = document.querySelector(".payment-modal");
+
+const render = () => {
+    let menuHtml = "";
+    menuArray.forEach((item) => {
+        menuHtml += `
+        
+        ``
+    })
+};

e66d7cfc872f45a1fd612ac9c939f560147fff13
Federico Celia <73991066+federicocelia@users.noreply.github.com>
2026-09-10 00:32:51 +0100

    Add complete menu items array and restyle menu layout

--- data.js
+++ data.js
@@ -1,23 +1,135 @@
 export const menuArray = [
-  {
-    name: "Pizza",
-    ingredients: ["pepperoni", "mushrom", "mozarella"],
-    id: 0,
-    price: 14,
-    emoji: "🍕",
-  },
+  // Mains
   {
     name: "Hamburger",
     ingredients: ["beef", "cheese", "lettuce"],
     price: 12,
     emoji: "🍔",
+    id: 0,
+  },
+  {
+    name: "Pizza",
+    ingredients: ["pepperoni", "mushroom", "mozzarella"],
+    price: 14,
+    emoji: "🍕",
     id: 1,
   },
   {
-    name: "Beer",
-    ingredients: ["grain, hops, yeast, water"],
-    price: 12,
-    emoji: "🍺",
+    name: "Hot Dog",
+    ingredients: ["sausage", "bun", "mustard"],
+    price: 8,
+    emoji: "🌭",
     id: 2,
+  },
+  {
+    name: "Taco",
+    ingredients: ["beef", "lettuce", "cheese"],
+    price: 10,
+    emoji: "🌮",
+    id: 3,
+  },
+  {
+    name: "Burrito",
+    ingredients: ["rice", "beans", "chicken"],
+    price: 13,
+    emoji: "🌯",
+    id: 4,
+  },
+  {
+    name: "Sandwich",
+    ingredients: ["ham", "cheese", "tomato"],
+    price: 9,
+    emoji: "🥪",
+    id: 5,
+  },
+  {
+    name: "Spaghetti",
+    ingredients: ["pasta", "tomato sauce", "parmesan"],
+    price: 15,
+    emoji: "🍝",
+    id: 6,
+  },
+  {
+    name: "Fried Chicken",
+    ingredients: ["chicken", "flour", "seasoning"],
+    price: 14,
+    emoji: "🍗",
+    id: 7,
+  },
+  {
+    name: "Steak",
+    ingredients: ["beef", "butter", "herbs"],
+    price: 24,
+    emoji: "🥩",
+    id: 8,
+  },
+  {
+    name: "Salad",
+    ingredients: ["lettuce", "tomato", "cucumber"],
+    price: 11,
+    emoji: "🥗",
+    id: 9,
+  },
+
+  // Sides
+  {
+    name: "French Fries",
+    ingredients: ["potatoes", "salt", "oil"],
+    price: 6,
+    emoji: "🍟",
+    id: 10,
+  },
+
+  // Desserts
+  {
+    name: "Ice Cream",
+    ingredients: ["milk", "sugar", "vanilla"],
+    price: 7,
+    emoji: "🍨",
+    id: 11,
+  },
+  {
+    name: "Doughnut",
+    ingredients: ["flour", "sugar", "glaze"],
+    price: 4,
+    emoji: "🍩",
+    id: 12,
+  },
+
+  // Drinks
+  {
+    name: "Water",
+    ingredients: ["spring water"],
+    price: 2,
+    emoji: "💧",
+    id: 13,
+  },
+  {
+    name: "Soft Drink",
+    ingredients: ["carbonated water", "sugar", "flavoring"],
+    price: 3,
+    emoji: "🍹",
+    id: 14,
+  },
+  {
+    name: "Coffee",
+    ingredients: ["coffee beans", "water"],
+    price: 3,
+    emoji: "☕",
+    id: 15,
+  },
+  {
+    name: "Milkshake",
+    ingredients: ["milk", "ice cream", "syrup"],
+    price: 8,
+    emoji: "🥤",
+    id: 16,
+  },
+  {
+    name: "Beer",
+    ingredients: ["grain", "hops", "yeast", "water"],
+    price: 5,
+    emoji: "🍺",
+    id: 17,
   },
 ];
